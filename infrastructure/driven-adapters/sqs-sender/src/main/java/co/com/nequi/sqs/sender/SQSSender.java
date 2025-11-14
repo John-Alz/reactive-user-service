27e1acefeec744b5a09098c3084c62d063c5a6d5
John Angel <jairoanngelll@gmail.com>
2025-11-14 11:57:32 -0500

    feat(sqs): Implementar publicador SQS para eventos de creación de usuario

--- infrastructure/driven-adapters/sqs-sender/src/main/java/co/com/nequi/sqs/sender/SQSSender.java
+++ infrastructure/driven-adapters/sqs-sender/src/main/java/co/com/nequi/sqs/sender/SQSSender.java
@@ -1,20 +1,28 @@
 package co.com.nequi.sqs.sender;
 
+import co.com.nequi.model.user.User;
+import co.com.nequi.model.user.gateways.UserEventRepository;
 import co.com.nequi.sqs.sender.config.SQSSenderProperties;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 import software.amazon.awssdk.services.sqs.model.SendMessageResponse;
 
+
 @Service
 @Log4j2
 @RequiredArgsConstructor
-public class SQSSender /*implements SomeGateway*/ {
+public class SQSSender implements UserEventRepository /*implements SomeGateway*/ {
     private final SQSSenderProperties properties;
     private final SqsAsyncClient client;
+    private final ObjectMapper objectMapper;
 
     public Mono<String> send(String message) {
         return Mono.fromCallable(() -> buildRequest(message))
@@ -28,5 +36,12 @@ public class SQSSender /*implements SomeGateway*/ {
                 .queueUrl(properties.queueUrl())
                 .messageBody(message)
                 .build();
+    }
+
+    @Override
+    public Mono<User> sendUserCreatedEvent(User user) {
+       return Mono.fromCallable(() -> objectMapper.writeValueAsString(user))
+               .flatMap(this::send)
+               .thenReturn(user);
     }
 }
